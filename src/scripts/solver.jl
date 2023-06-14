0f4fc6b80c4a95bb331d5a2bc9720beb7fd7c375
schev <schev@elektro.dtu.dk>
2023-06-14 17:54:57 +0200

    tt

--- src/scripts/solver.jl
+++ src/scripts/solver.jl
@@ -119,22 +119,22 @@ function compute_quasiGrad_solution_timed(InFile1::String, NewTimeLimitInSeconds
     # I1. load the system data
     jsn = quasiGrad.load_json(InFile1)
 
-    time_elapsed = start_time - time()
+    time_elapsed = time() - start_time
     println("I1: $(time_elapsed)")
 
     # I2. initialize the system
     adm, bit, cgd, ctb, ctd, flw, grd, idx, mgd, msc, ntk, prm, qG, scr,
     stt, sys, upd, wct = quasiGrad.base_initialization(jsn, Div=Division, hpc_params=true);
 
-    time_elapsed = start_time - time()
+    time_elapsed = time() - start_time
     println("I2: $(time_elapsed)")
 
-    qG.skip_ctg_eval = true
+    qG.skip_ctg_eval = false
 
     # I3. run an economic dispatch and update the states
     quasiGrad.economic_dispatch_initialization!(bit, cgd, ctb, ctd, flw, grd, idx, mgd, msc, ntk, prm, qG, scr, stt, sys, upd, wct)
 
-    time_elapsed = start_time - time()
+    time_elapsed = time() - start_time
     println("I3: $(time_elapsed)")
 
     # TT: time
@@ -156,25 +156,25 @@ function compute_quasiGrad_solution_timed(InFile1::String, NewTimeLimitInSeconds
         # L1. run power flow
         quasiGrad.solve_power_flow!(bit, cgd, grd, idx, mgd, msc, ntk, prm, qG, stt, sys, upd)
 
-        time_elapsed = start_time - time()
+        time_elapsed = time() - start_time
         println("L1: $(time_elapsed)")
 
         # L2. clean-up reserves by solving softly constrained LP
         quasiGrad.soft_reserve_cleanup!(idx, prm, qG, stt, sys, upd)
 
-        time_elapsed = start_time - time()
+        time_elapsed = time() - start_time
         println("L2: $(time_elapsed)")
 
         # L3. run adam
         quasiGrad.run_adam!(adm, bit, cgd, ctb, ctd, flw, grd, idx, mgd, msc, ntk, prm, qG, scr, stt, sys, upd, wct)
 
-        time_elapsed = start_time - time()
+        time_elapsed = time() - start_time
         println("L3: $(time_elapsed)")
 
         # L4. solve and apply projection
         quasiGrad.project!(pct_round, idx, prm, qG, stt, sys, upd, final_projection = false)
 
-        time_elapsed = start_time - time()
+        time_elapsed = time() - start_time
         println("L4: $(time_elapsed)")
 
         # L5. on the second-to-last iteration, fix the shunts; otherwise, just snap them
@@ -202,44 +202,44 @@ function compute_quasiGrad_solution_timed(InFile1::String, NewTimeLimitInSeconds
     # E1. run power flow, one more time
     quasiGrad.solve_power_flow!(bit, cgd, grd, idx, mgd, msc, ntk, prm, qG, stt, sys, upd)
 
-    time_elapsed = start_time - time()
+    time_elapsed = time() - start_time
     println("E1: $(time_elapsed)")
 
     # E2. clean-up reserves by solving softly constrained LP
     quasiGrad.soft_reserve_cleanup!(idx, prm, qG, stt, sys, upd)
 
-    time_elapsed = start_time - time()
+    time_elapsed = time() - start_time
     println("E2: $(time_elapsed)")
 
     # E3. run adam
     qG.adam_max_time = qG.adam_solve_times[end]
     quasiGrad.run_adam!(adm, bit, cgd, ctb, ctd, flw, grd, idx, mgd, msc, ntk, prm, qG, scr, stt, sys, upd, wct)
 
-    time_elapsed = start_time - time()
+    time_elapsed = time() - start_time
     println("E3: $(time_elapsed)")
 
     # E4. LP projection
     quasiGrad.project!(100.0, idx, prm, qG, stt, sys, upd, final_projection = true)
 
-    time_elapsed = start_time - time()
+    time_elapsed = time() - start_time
     println("E4: $(time_elapsed)")
     
     # E5. cleanup constrained powerflow
     quasiGrad.cleanup_constrained_pf_with_Gurobi!(idx, msc, ntk, prm, qG, stt, sys)
 
-    time_elapsed = start_time - time()
+    time_elapsed = time() - start_time
     println("E5: $(time_elapsed)")
 
     # E6. cleanup reserves
     quasiGrad.reserve_cleanup!(idx, prm, qG, stt, sys, upd)
 
-    time_elapsed = start_time - time()
+    time_elapsed = time() - start_time
     println("E6: $(time_elapsed)")
 
     # E7. write the final solution
     quasiGrad.write_solution("solution.jl", prm, qG, stt, sys)
 
-    time_elapsed = start_time - time()
+    time_elapsed = time() - start_time
     println("E7: $(time_elapsed)")
 
     # E8. post process
