2ae1b77db4088f20d5d8baecceb0602299a595c6
schev <schev@elektro.dtu.dk>
2023-06-16 09:12:09 +0200

    message

--- src/scripts/solver.jl
+++ src/scripts/solver.jl
@@ -161,43 +161,45 @@ function compute_quasiGrad_solution_timed(InFile1::String, NewTimeLimitInSeconds
     
     quasiGrad.write_solution("solution.jl", prm, qG, stt, sys)
 
-    ## loop and solve: adam -> projection -> IBR
-    #n_its = length(qG.pcts_to_round)
-    #for (solver_itr, pct_round) in enumerate(qG.pcts_to_round)
-#
-    #    # TT: set an adam solve time
-    #    qG.adam_max_time = qG.adam_solve_times[solver_itr]
-#
-    #    # L1. run power flow
-    #    quasiGrad.solve_power_flow!(bit, cgd, grd, idx, mgd, msc, ntk, prm, qG, stt, sys, upd)
-#
-    #    time_elapsed = time() - start_time
-    #    println("L1: $(time_elapsed)")
-#
-    #    # L2. clean-up reserves by solving softly constrained LP
-    #    quasiGrad.soft_reserve_cleanup!(idx, prm, qG, stt, sys, upd)
-#
-    #    time_elapsed = time() - start_time
-    #    println("L2: $(time_elapsed)")
-#
-    #    # L3. run adam
-    #    quasiGrad.run_adam!(adm, bit, cgd, ctb, ctd, flw, grd, idx, mgd, msc, ntk, prm, qG, scr, stt, sys, upd, wct)
-#
-    #    time_elapsed = time() - start_time
-    #    println("L3: $(time_elapsed)")
-#
-    #    # L4. solve and apply projection
-    #    quasiGrad.project!(pct_round, idx, prm, qG, stt, sys, upd, final_projection = false)
-#
-    #    time_elapsed = time() - start_time
-    #    println("L4: $(time_elapsed)")
-#
-    #    # L5. on the second-to-last iteration, fix the shunts; otherwise, just snap them
-    #    fix = solver_itr == (n_its-1)
-    #    quasiGrad.snap_shunts!(fix, prm, qG, stt, upd)
-    #end
+    # loop and solve: adam -> projection -> IBR
+    n_its = length(qG.pcts_to_round)
+    for (solver_itr, pct_round) in enumerate(qG.pcts_to_round)
+
+        # TT: set an adam solve time
+        qG.adam_max_time = qG.adam_solve_times[solver_itr]
+
+        # L1. run power flow
+        quasiGrad.solve_power_flow!(bit, cgd, grd, idx, mgd, msc, ntk, prm, qG, stt, sys, upd)
+
+        time_elapsed = time() - start_time
+        println("L1: $(time_elapsed)")
+
+        # L2. clean-up reserves by solving softly constrained LP
+        quasiGrad.soft_reserve_cleanup!(idx, prm, qG, stt, sys, upd)
+
+        time_elapsed = time() - start_time
+        println("L2: $(time_elapsed)")
+
+        # L3. run adam
+        quasiGrad.run_adam!(adm, bit, cgd, ctb, ctd, flw, grd, idx, mgd, msc, ntk, prm, qG, scr, stt, sys, upd, wct)
+
+        time_elapsed = time() - start_time
+        println("L3: $(time_elapsed)")
+
+        # L4. solve and apply projection
+        quasiGrad.project!(pct_round, idx, prm, qG, stt, sys, upd, final_projection = false)
+
+        time_elapsed = time() - start_time
+        println("L4: $(time_elapsed)")
+
+        # L5. on the second-to-last iteration, fix the shunts; otherwise, just snap them
+        fix = solver_itr == (n_its-1)
+        quasiGrad.snap_shunts!(fix, prm, qG, stt, upd)
+    end
     ###############################################################
     ###############################################################
+
+    println("made it here!")
 #
     ## Now, we're in the End Game.
     ##
