8b7b05a2028a4408f28b797ac66274a222363e8e
schev <schev@elektro.dtu.dk>
2023-08-19 22:06:20 -0400

    updates

--- src/core/initializations.jl
+++ src/core/initializations.jl
@@ -18,7 +18,7 @@ function initialize_qG(prm::quasiGrad.Param; Div::Int64=1, hpc_params::Bool=fals
         print_freq                    = 5    # (i.e., every how often)
         print_final_stats             = true # print stats at the end?
         print_lbfgs_iterations        = true
-        print_projection_success      = true
+        print_projection_success      = false # too much :)
         print_linear_pf_iterations    = true
         print_reserve_cleanup_success = true
     end
@@ -965,55 +965,24 @@ function initialize_ctg(stt::quasiGrad.State, sys::quasiGrad.System, prm::quasiG
     # Note: Ybr should be sparse!! otherwise,
     # the sparse preconditioner won't have any memory limits and
     # will be the full Chol-decomp -- not a big deal, I guess..
-    if qG.base_solver == "pcg"
-        if sys.nb <= qG.min_buses_for_krylov
-            # too few buses -- use LU
-            println("Not enough buses for Krylov! Using LU for all Ax=b system solves.")
-        end
+    #
+    # time is short -- let's jsut always use ldl preconditioner -- it's just as fast
+    Ybr_ChPr = quasiGrad.Preconditioners.lldl(Ybr, memory = qG.cutoff_level);
 
-        # time is short -- let's jsut always use ldl preconditioner -- it's just as fast
-        Ybr_ChPr = quasiGrad.Preconditioners.lldl(Ybr, memory = qG.cutoff_level);
-
-        # OG#2 solution!
-            # can we build cholesky?
-            # if minimum(ac_b_params) < 0.0
-            #     Ybr_ChPr = quasiGrad.CholeskyPreconditioner(Ybr, qG.cutoff_level);
-            # else
-            #     @info "Unsing an ldl preconditioner."
-            #     Ybr_ChPr = quasiGrad.ldl(Ybr, qG.cutoff_level);
-            # end
-
-        # OG#1 solution!
-            # # test for negative reactances -- @info "Preconditioning is disabled."
-            # if minimum(ac_b_params) < 0.0
-            #     # Amrit Pandey: "watch out for negatvive reactance! You will lose
-            #     #                pos-sem-def of the Cholesky preconditioner."
-            #     abs_b    = abs.(ac_b_params)
-            #     abs_Ybr  = (E'*quasiGrad.spdiagm(abs_b)*E)[2:end,2:end] 
-            #     Ybr_ChPr = quasiGrad.CholeskyPreconditioner(abs_Ybr, qG.cutoff_level)
-            # else
-            #     Ybr_ChPr = quasiGrad.CholeskyPreconditioner(Ybr, qG.cutoff_level);
-            # end
-    else
-        # time is short -- let's jsut always use ldl preconditioner -- it's just as fast
-        Ybr_ChPr = quasiGrad.Preconditioners.lldl(Ybr, memory = qG.cutoff_level);
-            # # => Ybr_ChPr = quasiGrad.I
-            # Ybr_ChPr = quasiGrad.CholeskyPreconditioner(Ybr, qG.cutoff_level);
+    # warn, if too few buses
+    if sys.nb <= qG.min_buses_for_krylov
+        # too few buses -- use LU
+        println("Not enough buses for Krylov! Using LU for all Ax=b system solves.")
     end
     
     # should we build the cholesky decomposition of the base case
     # admittance matrix? we build this to compute high-fidelity
     # solutions of the rank-1 update matrices
-    if qG.build_basecase_cholesky
-        if minimum(ac_b_params) < 0.0
-            @info "Yb not PSd -- using ldlt (instead of cholesky) to construct WMI update vectors."
-            Ybr_Ch = quasiGrad.ldlt(Ybr)
-        else
-            Ybr_Ch = quasiGrad.cholesky(Ybr)
-        end
+    if minimum(ac_b_params) < 0.0
+        @info "Yb not PSd -- using ldlt (instead of cholesky) to construct WMI update vectors."
+        Ybr_Ch = quasiGrad.ldlt(Ybr)
     else
-        # this is nonsense
-        Ybr_Ch = quasiGrad.I
+        Ybr_Ch = quasiGrad.cholesky(Ybr)
     end
 
     # get the flow matrix
@@ -2545,4 +2514,120 @@ function initialize_lbfgs(mgd::quasiGrad.MasterGrad, prm::quasiGrad.Param, qG::q
     lbfgs = quasiGrad.LBFGS(p0, state, diff, idx, map, step, zpf)
 
     return lbfgs
+end
+
+# %%
+# build everything that will be needed to solve ctgs
+function initialize_ctg_test(stt::quasiGrad.State, sys::quasiGrad.System, prm::quasiGrad.Param, qG::quasiGrad.QG, idx::quasiGrad.Index)
+    # note, the reference bus is always bus #1
+    #
+    # first, get the ctg limits
+    s_max_ctg = [prm.acline.mva_ub_em; prm.xfm.mva_ub_em]
+
+    # get the ordered names of all components
+    ac_ids = [prm.acline.id; prm.xfm.id ]
+
+    # get the ordered (negative!!) susceptances
+    ac_b_params = -[prm.acline.b_sr; prm.xfm.b_sr]
+    
+    # build the full incidence matrix: E = lines x buses
+    E, Efr, Eto = build_incidence(idx, prm, stt, sys)
+    Er = E[:,2:end]
+    ErT = copy(Er')
+
+    # get the diagonal admittance matrix   => Ybs == "b susceptance"
+    Ybs = quasiGrad.spdiagm(ac_b_params)
+    Yb  = E'*Ybs*E
+    Ybr = Yb[2:end,2:end]  # use @view ? 
+
+    # should we precondition the base case?
+    #
+    # Note: Ybr should be sparse!! otherwise,
+    # the sparse preconditioner won't have any memory limits and
+    # will be the full Chol-decomp -- not a big deal, I guess..
+    if qG.base_solver == "pcg"
+        if sys.nb <= qG.min_buses_for_krylov
+            # too few buses -- use LU
+            println("Not enough buses for Krylov! Using LU for all Ax=b system solves.")
+        end
+
+        # time is short -- let's jsut always use ldl preconditioner -- it's just as fast
+        Ybr_ChPr = quasiGrad.Preconditioners.lldl(Ybr, memory = qG.cutoff_level);
+    else
+        # time is short -- let's jsut always use ldl preconditioner -- it's just as fast
+        Ybr_ChPr = quasiGrad.Preconditioners.lldl(Ybr, memory = qG.cutoff_level);
+            # # => Ybr_ChPr = quasiGrad.I
+            # Ybr_ChPr = quasiGrad.CholeskyPreconditioner(Ybr, qG.cutoff_level);
+    end
+    
+    # should we build the cholesky decomposition of the base case
+    # admittance matrix? we build this to compute high-fidelity
+    # solutions of the rank-1 update matrices
+    if qG.build_basecase_cholesky
+        if minimum(ac_b_params) < 0.0
+            @info "Yb not PSd -- using ldlt (instead of cholesky) to construct WMI update vectors."
+            Ybr_Ch = quasiGrad.ldlt(Ybr)
+        else
+            Ybr_Ch = quasiGrad.cholesky(Ybr)
+        end
+    else
+        # this is nonsense
+        Ybr_Ch = quasiGrad.I
+    end
+
+    # get the flow matrix
+    Yfr  = Ybs*Er
+    YfrT = copy(Yfr')
+
+    # build the low-rank contingecy updates
+    #
+    # base: Y_b*theta_b = p
+    # ctg:  Y_c*theta_c = p
+    #       Y_c = Y_b + uk'*uk
+    ctg_out_ind = Dict(ctg_ii => Vector{Int64}(undef, length(prm.ctg.components[ctg_ii])) for ctg_ii in 1:sys.nctg)
+    ctg_params  = Dict(ctg_ii => Vector{Float64}(undef, length(prm.ctg.components[ctg_ii])) for ctg_ii in 1:sys.nctg)
+    
+    # should we build the full ctg matrices?
+    #
+    # build something small of the correct data type
+    Ybr_k = Dict(1 => quasiGrad.spzeros(1,1))
+
+    # and/or, should we build the low rank ctg elements?
+    #
+    # no need => v_k = Dict(ctg_ii => quasiGrad.spzeros(nac) for ctg_ii in 1:sys.nctg)
+    # no need => b_k = Dict(ctg_ii => 0.0 for ctg_ii in 1:sys.nctg)
+    u_k = [zeros(sys.nb-1) for ctg_ii in 1:sys.nctg] # Dict(ctg_ii => zeros(sys.nb-1) for ctg_ii in 1:sys.nctg) # Dict(ctg_ii => quasiGrad.spzeros(sys.nb-1) for ctg_ii in 1:sys.nctg)
+    g_k = zeros(sys.nctg) # => Dict(ctg_ii => 0.0 for ctg_ii in 1:sys.nctg)
+    z_k = [zeros(sys.nac) for ctg_ii in 1:sys.nctg]
+    # if the "w_k" formulation is wanted => w_k = Dict(ctg_ii => quasiGrad.spzeros(sys.nb-1) for ctg_ii in 1:sys.nctg)
+
+    # loop over components (see below for comments!!!)
+    for ctg_ii in 1:sys.nctg
+        cmpnts = prm.ctg.components[ctg_ii]
+        for (cmp_ii,cmp) in enumerate(cmpnts)
+            cmp_index = findfirst(x -> x == cmp, ac_ids) 
+            ctg_out_ind[ctg_ii][cmp_ii] = cmp_index
+            ctg_params[ctg_ii][cmp_ii]  = -ac_b_params[cmp_index]
+        end
+    end
+
+    # solve for the wmi factors!
+    zrs = zeros(qG.num_threads, sys.nb-1)
+    Threads.@threads for ctg_ii in 1:sys.nctg
+        # u_k[ctg_ii] .= Ybr_Ch\Er[ctg_out_ind[ctg_ii][1],:]
+
+        quasiGrad.cg!(u_k[ctg_ii], Ybr, Er[ctg_out_ind[ctg_ii][1],:], abstol = qG.pcg_tol, Pl=Ybr_ChPr, maxiter = qG.max_pcg_its)
+        
+    end
+    t1 = time()
+
+    Threads.@threads for ctg_ii in 1:sys.nctg
+        # this code is optimized -- see above for comments!!!
+        #u_k[ctg_ii]        .= Ybr_Ch\Er[ctg_out_ind[ctg_ii][1],:]
+
+        @turbo g_k[ctg_ii]  = -ac_b_params[ctg_out_ind[ctg_ii][1]]/(1.0+(quasiGrad.dot(Er[ctg_out_ind[ctg_ii][1],:],u_k[ctg_ii]))*-ac_b_params[ctg_out_ind[ctg_ii][1]])
+        @turbo mul!(z_k[ctg_ii], Yfr, u_k[ctg_ii])
+    end
+    t_ctg = time() - t1
+    println("WMI factor time: $t_ctg")
 end
--- test/t29_parallel_ED_test.jl
+++ test/t29_parallel_ED_test.jl
@@ -24,3 +24,168 @@ qG.print_projection_success = false
 
 quasiGrad.economic_dispatch_initialization!(cgd, ctg, flw, grd, idx, mgd, ntk, prm, qG, scr, stt, sys, upd)
 stt0 = deepcopy(stt);
+
+
+# %% test -- wmi factor acceleration!!!
+Base.GC.gc()
+
+@time quasiGrad.initialize_ctg_test(stt, sys, prm, qG, idx)
+
+# %% =============
+E, Efr, Eto = quasiGrad.build_incidence(idx, prm, stt, sys)
+Er = E[:,2:end]
+ErT = copy(Er')
+
+
+
+
+# %% === ========
+
+# note, the reference bus is always bus #1
+#
+# first, get the ctg limits
+s_max_ctg = [prm.acline.mva_ub_em; prm.xfm.mva_ub_em]
+
+# get the ordered names of all components
+ac_ids = [prm.acline.id; prm.xfm.id ]
+
+# get the ordered (negative!!) susceptances
+ac_b_params = -[prm.acline.b_sr; prm.xfm.b_sr]
+
+# build the full incidence matrix: E = lines x buses
+E, Efr, Eto = quasiGrad.build_incidence(idx, prm, stt, sys)
+Er = E[:,2:end]
+ErT = copy(Er')
+
+# get the diagonal admittance matrix   => Ybs == "b susceptance"
+Ybs = quasiGrad.spdiagm(ac_b_params)
+Yb  = E'*Ybs*E
+Ybr = Yb[2:end,2:end]  # use @view ? 
+
+# should we precondition the base case?
+#
+# Note: Ybr should be sparse!! otherwise,
+# the sparse preconditioner won't have any memory limits and
+# will be the full Chol-decomp -- not a big deal, I guess..
+if qG.base_solver == "pcg"
+    if sys.nb <= qG.min_buses_for_krylov
+        # too few buses -- use LU
+        println("Not enough buses for Krylov! Using LU for all Ax=b system solves.")
+    end
+
+    # time is short -- let's jsut always use ldl preconditioner -- it's just as fast
+    Ybr_ChPr = quasiGrad.Preconditioners.lldl(Ybr, memory = qG.cutoff_level);
+else
+    # time is short -- let's jsut always use ldl preconditioner -- it's just as fast
+    Ybr_ChPr = quasiGrad.Preconditioners.lldl(Ybr, memory = qG.cutoff_level);
+        # # => Ybr_ChPr = quasiGrad.I
+        # Ybr_ChPr = quasiGrad.CholeskyPreconditioner(Ybr, qG.cutoff_level);
+end
+
+# should we build the cholesky decomposition of the base case
+# admittance matrix? we build this to compute high-fidelity
+# solutions of the rank-1 update matrices
+if qG.build_basecase_cholesky
+    if minimum(ac_b_params) < 0.0
+        @info "Yb not PSd -- using ldlt (instead of cholesky) to construct WMI update vectors."
+        Ybr_Ch = quasiGrad.ldlt(Ybr)
+    else
+        Ybr_Ch = quasiGrad.cholesky(Ybr)
+    end
+else
+    # this is nonsense
+    Ybr_Ch = quasiGrad.I
+end
+
+# get the flow matrix
+Yfr  = Ybs*Er
+YfrT = copy(Yfr')
+
+# build the low-rank contingecy updates
+#
+# base: Y_b*theta_b = p
+# ctg:  Y_c*theta_c = p
+#       Y_c = Y_b + uk'*uk
+ctg_out_ind = Dict(ctg_ii => Vector{Int64}(undef, length(prm.ctg.components[ctg_ii])) for ctg_ii in 1:sys.nctg)
+ctg_params  = Dict(ctg_ii => Vector{Float64}(undef, length(prm.ctg.components[ctg_ii])) for ctg_ii in 1:sys.nctg)
+
+# should we build the full ctg matrices?
+#
+# build something small of the correct data type
+Ybr_k = Dict(1 => quasiGrad.spzeros(1,1))
+
+# and/or, should we build the low rank ctg elements?
+#
+# no need => v_k = Dict(ctg_ii => quasiGrad.spzeros(nac) for ctg_ii in 1:sys.nctg)
+# no need => b_k = Dict(ctg_ii => 0.0 for ctg_ii in 1:sys.nctg)
+u_k = [zeros(sys.nb-1) for ctg_ii in 1:sys.nctg] # Dict(ctg_ii => zeros(sys.nb-1) for ctg_ii in 1:sys.nctg) # Dict(ctg_ii => quasiGrad.spzeros(sys.nb-1) for ctg_ii in 1:sys.nctg)
+g_k = zeros(sys.nctg) # => Dict(ctg_ii => 0.0 for ctg_ii in 1:sys.nctg)
+z_k = [zeros(sys.nac) for ctg_ii in 1:sys.nctg]
+# if the "w_k" formulation is wanted => w_k = Dict(ctg_ii => quasiGrad.spzeros(sys.nb-1) for ctg_ii in 1:sys.nctg)
+
+# loop over components (see below for comments!!!)
+for ctg_ii in 1:sys.nctg
+    cmpnts = prm.ctg.components[ctg_ii]
+    for (cmp_ii,cmp) in enumerate(cmpnts)
+        cmp_index = findfirst(x -> x == cmp, ac_ids) 
+        ctg_out_ind[ctg_ii][cmp_ii] = cmp_index
+        ctg_params[ctg_ii][cmp_ii]  = -ac_b_params[cmp_index]
+    end
+end
+
+
+
+# %% ==============
+ctg_ii = 1
+zrs = zeros(sys.nb-1)
+zrs .= @view Er[ctg_out_ind[ctg_ii][1],:]
+# %%
+quasiGrad.cg!(u_k[ctg_ii], Ybr, zrs, abstol = qG.pcg_tol, Pl=Ybr_ChPr, maxiter = qG.max_pcg_its)
+
+# %% ==============
+lck = Threads.SpinLock() # => Threads.ReentrantLock(); SpinLock slower, but safer, than ReentrantLock ..?
+
+# define a vector of bools acciated with multithread storage:
+ready_to_use = ones(Bool, qG.num_threads+2)
+ready_to_use .= true
+lck = Threads.SpinLock()
+
+                            ## solve for the wmi factors!
+                            #Threads.@threads for ctg_ii in 1:sys.nctg
+                            #    # u_k[ctg_ii] .= Ybr_Ch\Er[ctg_out_ind[ctg_ii][1],:]
+                            #
+                            #    quasiGrad.cg!(u_k[ctg_ii], Ybr, Er[ctg_out_ind[ctg_ii][1],:], abstol = qG.pcg_tol, Pl=Ybr_ChPr, maxiter = qG.max_pcg_its)
+                            #    
+                            #end
+
+zrs = zeros(sys.nb-1, qG.num_threads+2)
+
+t1 = time()
+
+Threads.@threads for ctg_ii in 1:sys.nctg
+    # use a custom "thread ID" -- three indices: tii, ctg_ii, and thrID
+    thrID = Int16(1)
+    Threads.lock(lck)
+        thrIdx = findfirst(ready_to_use)
+        if thrIdx != Nothing
+            thrID = Int16(thrIdx)
+        end
+        ready_to_use[thrID] = false # now in use :)
+    Threads.unlock(lck)
+
+    # zrs[thrID] .= @view Er[ctg_out_ind[ctg_ii][1],:]
+    
+    # this code is optimized -- see above for comments!!!
+    # u_k[ctg_ii]        .= Ybr_Ch\Er[ctg_out_ind[ctg_ii][1],:]
+    #quasiGrad.cg!(u_k[ctg_ii], Ybr, zrs[thrID], abstol = qG.pcg_tol, Pl=Ybr_ChPr, maxiter = qG.max_pcg_its)
+    #quasiGrad.@turbo g_k[ctg_ii]  = -ac_b_params[ctg_out_ind[ctg_ii][1]]/(1.0+(quasiGrad.dot(Er[ctg_out_ind[ctg_ii][1],:],u_k[ctg_ii]))*-ac_b_params[ctg_out_ind[ctg_ii][1]])
+    #quasiGrad.@turbo mul!(z_k[ctg_ii], Yfr, u_k[ctg_ii])
+
+    # all done!!
+    Threads.lock(lck)
+        ctg.ready_to_use[thrID] = true
+    Threads.unlock(lck)
+end
+
+t_ctg = time() - t1
+println("WMI factor time: $t_ctg")
