ea04f9fe6ea6b4cfb07d4c66d2451ae7e4cbbed1
schev <schev@elektro.dtu.dk>
2023-08-29 14:30:52 -0400

    updates

--- test/t31_test_solvers.jl
+++ test/t31_test_solvers.jl
@@ -16,5 +16,39 @@ quasiGrad.pc("./src/precompile_37bus.json", NewTimeLimitInSeconds, Division, Net
 tfp = "C:/Users/Samuel.HORACE/Dropbox (Personal)/Documents/Julia/GO3_testcases/"
 
 # %% call the solver!
-InFile1               = tfp*"C3E3.1_20230629/D1/C3E3N00617D1/scenario_001.json"
+t0 = time()
+InFile1 = tfp*"C3E3.1_20230629/D1/C3E3N00617D1/scenario_001.json"
 quasiGrad.compute_quasiGrad_solution_d1(InFile1, NewTimeLimitInSeconds, Division, NetworkModel, AllowSwitching; post_process=true)
+tf = time() - t0
+println("total time: $tf")
+
+t0 = time()
+InFile1 = tfp*"C3E3.1_20230629/D1/C3E3N01576D1/scenario_027.json"
+quasiGrad.compute_quasiGrad_solution_d1(InFile1, NewTimeLimitInSeconds, Division, NetworkModel, AllowSwitching; post_process=true)
+tf = time() - t0
+println("total time: $tf")
+
+t0 = time()
+InFile1 = tfp*"C3E3.1_20230629/D1/C3E3N04224D1/scenario_143.json"
+quasiGrad.compute_quasiGrad_solution_d1(InFile1, NewTimeLimitInSeconds, Division, NetworkModel, AllowSwitching; post_process=true)
+tf = time() - t0
+println("total time: $tf")
+
+t0 = time()
+InFile1 = tfp*"C3E3.1_20230629/D1/C3E3N06049D1/scenario_043.json"
+quasiGrad.compute_quasiGrad_solution_d1(InFile1, NewTimeLimitInSeconds, Division, NetworkModel, AllowSwitching; post_process=true)
+tf = time() - t0
+println("total time: $tf")
+
+t0 = time()
+InFile1 = tfp*"C3E3.1_20230629/D1/C3E3N06717D1/scenario_031.json"
+quasiGrad.compute_quasiGrad_solution_d1(InFile1, NewTimeLimitInSeconds, Division, NetworkModel, AllowSwitching; post_process=true)
+tf = time() - t0
+println("total time: $tf")
+
+t0 = time()
+InFile1 = tfp*"C3E3.1_20230629/D1/C3E3N08316D1/scenario_001.json"
+quasiGrad.compute_quasiGrad_solution_d1(InFile1, NewTimeLimitInSeconds, Division, NetworkModel, AllowSwitching; post_process=true)
+tf = time() - t0
+println("total time: $tf")
+
