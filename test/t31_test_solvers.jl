043b6c7b88db33c100077f1ce3673bcbdf3e9288
schev <schev@elektro.dtu.dk>
2023-08-30 16:28:24 -0400

    updates

--- test/t31_test_solvers.jl
+++ test/t31_test_solvers.jl
@@ -10,6 +10,8 @@ NewTimeLimitInSeconds = 600.0
 Division              = 1
 NetworkModel          = "test"
 AllowSwitching        = 1
+
+# %%
 pc("./src/precompile_37bus.json", NewTimeLimitInSeconds, Division, NetworkModel, AllowSwitching)
 
 # common folder for calling
