aaf2c50c8f408bad41f918de1583fb79f70652a6
schev <schev@elektro.dtu.dk>
2023-08-31 20:20:21 -0400

    updates

--- warmup.jl
+++ warmup.jl
@@ -25,4 +25,4 @@ include("./warmup_run.jl")
 
 # create system image
 using PackageCompiler
-create_sysimage(sysimage_path="/qfs/projects/goc/share/apps/Julia/julia-1.10.0-beta1-SamChevalier/share/julia/site/SamChevalierQG2.so", precompile_execution_file="warmup_run.jl")
+create_sysimage(sysimage_path="/qfs/projects/goc/share/apps/Julia/julia-1.10.0-beta1-SamChevalier/share/julia/site/SamChevalierQG3.so", precompile_execution_file="warmup_run.jl")
