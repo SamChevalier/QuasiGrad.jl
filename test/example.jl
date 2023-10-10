822c669d5e5349eb2bc5a09bc04cba63e33a3675
SamChevalier <chev856@gmail.com>
2023-10-10 16:56:12 -0400

    updates

--- test/example.jl
+++ test/example.jl
@@ -1,7 +1,7 @@
 using quasiGrad
 
 # test a dvision 1 test case
-InFile                = "../data/C3E3.1_20230629/D1/C3E3N00617D1/scenario_001.json"
+InFile                = "./data/C3E3.1_20230629/D1/C3E3N00617D1/scenario_001.json"
 NewTimeLimitInSeconds = 600.0
 Division              = 1
 NetworkModel          = "test"
