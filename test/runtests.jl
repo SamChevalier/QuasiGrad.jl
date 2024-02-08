38e2d9a8d757c18ca65ef8a986ad7ee9d76dbc75
SamChevalier <chev856@gmail.com>
2024-02-08 16:44:01 -0500

    updates

---
+++ test/runtests.jl
@@ -0,0 +1,7 @@
+@testset "QuasiGrad.jl" begin
+    @testset "basic test" begin
+
+        # random test
+        @test 1 == 1
+    end
+end
--- test/test_QuasiGrad.jl
+++ test/test_QuasiGrad.jl
@@ -1,4 +1,4 @@
-@testset "test nfa opb" begin
+@testset "test " begin
 
     # define test
     InFile                = "./data/C3E3.1_20230629/D1/C3E3N00617D1/scenario_001.json"
@@ -11,7 +11,4 @@
     adm, cgd, ctg, flw, grd, idx, lbf, mgd, ntk, prm, qG, scr, stt, sys, upd = 
         QuasiGrad.base_initialization(jsn, Div=Division, hpc_params=true, line_switching=AllowSwitching);
     QuasiGrad.economic_dispatch_initialization!(cgd, ctg, flw, grd, idx, mgd, ntk, prm, qG, scr, stt, sys, upd)
-    
-    #@test scr.4.524899419595314e7
-
 end
